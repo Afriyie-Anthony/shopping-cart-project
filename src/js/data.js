4c5b7496e5fb721a1bc39d572ff7e874d320c181
Afriyie-Anthony <kobbydalton@icloud.com>
2025-01-13 10:49:52 +0000

    add to cart functionality

--- src/js/data.js
+++ src/js/data.js
@@ -11,49 +11,49 @@ let shopItemsData = [
   "name": "Mens  Fit T-Shirts ",
   "price": 22.3,
   "desc": "Lorem ipsum dolor sit amet consectetur adipisicing ",
-  "img": "./../public/images/product_2.png",
+  "img": "../../public/images/product_2.png",
  
 }, {
   "id": 3,
   "name": "Mens Cotton Jacket",
   "price": 55.99,
   "desc": "Lorem ipsum dolor sit amet consectetur adipisicing ",
-  "img": "./../public/images/product_2.png"
+  "img": "../../public/images/product_2.png"
  
 }, {
   "id": 4,
   "name": "Mens Casual Slim Fit",
   "price": 15.99,
   "desc": "Lorem ipsum dolor sit amet consectetur adipisicing ",
-  "img": "./../public/images/product_10.png"
+  "img": "../../public/images/product_10.png"
   
 }, {
   "id": 5,
   "name": "Solid Gold Petite Micropave ",
   "price": 168,
   "desc": "Lorem ipsum dolor sit amet consectetur adipisicing ",
-  "img": "./../public/images/product_4_1.png"
+  "img": "../../public/images/product_4_1.png"
   
 }, {
   "id": 6,
   "name": "Solid Gold Petite Micropave ",
   "price": 168,
   "desc": "Lorem ipsum dolor sit amet consectetur adipisicing ",
-  "img": "./../public/images/product_5.png"
+  "img": "../../public/images/product_5.png"
   
 }, {
   "id": 7,
   "name": "White Gold Plated Princess",
   "price": 9.99,
   "desc": "Lorem ipsum dolor sit amet consectetur adipisicing ",
-  "img": "./../public/images/product_6.png"
+  "img": "../../public/images/product_6.png"
 
 }, {
   "id": 8,
   "name": "Pierced Owl  Steel Double",
   "price": 10.99,
   "desc": "Lorem ipsum dolor sit amet consectetur adipisicing ",
-  "img": "./../public/images/product_7.png"
+  "img": "../../public/images/product_7.png"
  
 }
 ]
